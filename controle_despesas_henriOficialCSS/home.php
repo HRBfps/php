d94a223dde1cd5df9908a1707ef2936b8aa9ac28
HRB.7 <84103822+HRBfps@users.noreply.github.com>
2023-10-03 08:36:27 -0300

    finalmente CSS

---
+++ controle_despesas_henriOficialCSS/home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Despesas</title>
+    <link rel="stylesheet" href="estilos_menu.css">
+</head>
+<body>
+    <?php
+        require "menu.php"; // Importa o menu do sistema de Controle de Despesas
+    ?>
+    <h3>CONTROLE DE DESPESAS</h3>
+    <div class="foto">
+        <img src="imagens/despesas.png" alt="Controle de Despesas"> 
+    </div>
+    <h4>O que é um controle de despesas?</h4>
+    <p>
+    O controle de gastos é uma <strong>parte importante do planejamento financeiro</strong>, pois ajuda as pessoas a gerenciar 
+    suas <i>finanças</i> e <strong>evitar</strong> <i>gastos</i> desnecessários. Ele envolve rastrear suas despesas e compará-las com suas receitas para ver onde estão sendo gastos os recursos.</p>
+    <footer>HENRI.MP4 - 2023&copy;</footer>
+</body>
+</html>
